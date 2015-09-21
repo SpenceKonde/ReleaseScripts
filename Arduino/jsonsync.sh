9c2e71957962dedf1036795cf1d8f1861413cc6a
Spence Konde <spencekonde@gmail.com>
2015-09-21 00:56:34 -0400

    Update jsonsync.sh

--- Arduino/jsonsync.sh
+++ Arduino/jsonsync.sh
@@ -2,6 +2,6 @@
 
 echo "Start automatic board manager json file sync..."
 rm -rf /var/boardmgr/scratch
-git sync https://github.com/SpenceKonde/ReleaseScripts /var/boardmgr/scratch
+git clone https://github.com/SpenceKonde/ReleaseScripts /var/boardmgr/scratch
 
 cp /var/boardmgr/scratch *.json /var/www/html/
